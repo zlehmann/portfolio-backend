ce32c36d41a4657c0e82b0395f08dc107fd5a700
Zachary Lehmann <zlehmann@gmail.com>
2019-09-30 20:32:55 -0400

    adding test blurb to database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,15 @@ Project.create([
       title: "Fetch My Corgi",
       proj_type: "Technology",
       card_image: "fetch-my-corgi.png",
-      blurb: "A corgi adoption aggregation app to help users find a corgi to adopt."
+      blurb: "A corgi adoption aggregation app to help users find a corgi to adopt.
+      My partner and I have wanted to adopt a corgi for years; but we've always
+      worried about haveing enough time, money, and space to give a dog the home it
+      deserves. Now we are finally ready!
+      (\n)
+      But corgis are super popular and it is a real hassle trying to stay on top of
+      all the adoption websites out there looking for the next short-legged pup to
+      come along! So I developed this app to help us scrape as many adoption sites as
+      possible to help us find the next furry addition to our family"
     },
     {
       title: "Virtual Fly Box",
